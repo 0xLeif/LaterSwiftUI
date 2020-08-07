4e5be9c293842270c5c7d4a002f612dc8776e92d
zmeriksen <zmeriksen@icloud.com>
2020-08-06 19:20:27 -0500

    Initial Commit

---
+++ LaterSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LaterSwiftUI
+//
+//  Created by Zach Eriksen on 8/6/20.
+//  Copyright © 2020 oneleif. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
